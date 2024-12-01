63cebb0375abc43c714f86f6975669e6222e29ce
laobamac <wxcznb@qq.com>
2024-12-01 12:41:01 +0800

    Reuse the OpenCore 1.0.2 and fix SLC folder,#3

--- oclp_mod/constants.py
+++ oclp_mod/constants.py
@@ -28,7 +28,7 @@ class Constants:
 
         # OpenCore Versioning
         # https://github.com/acidanthera/OpenCorePkg
-        self.opencore_version: str = "1.0.3"
+        self.opencore_version: str = "1.0.2"
 
         # Kext Versioning
         ## Acidanthera
