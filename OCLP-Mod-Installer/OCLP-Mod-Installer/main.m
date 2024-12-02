e8836fda6f1464e63d03c95424c7c638c22250bd
laobamac <wxcznb@qq.com>
2024-12-02 23:05:13 +0800

    Add installer open-sources

---
+++ OCLP-Mod-Installer/OCLP-Mod-Installer/main.m
@@ -0,0 +1,24 @@
+/*
+Copyright © 2024 王孝慈
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#import <Cocoa/Cocoa.h>
+#import "Handler.h"
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        setpgid(0, 0);
+        NSApplication * app = [NSApplication sharedApplication];
+        Handler *h = [[Handler alloc] init];
+        [app setDelegate:h];
+        [app run];
+    }
+    return 0;
+}
